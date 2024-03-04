60178160acfe21608994eb17afbfb0475e35a484
Daniel Westergren <westis@gmail.com>
2024-03-04 23:30:43 +0100

    Added API definition

---
+++ api/endpoints.ts
@@ -0,0 +1,10 @@
+// api/endpoints.ts
+/* eslint-disable babel/require-config-file */
+export const API_ENDPOINTS = {
+  events: "/mcalendar.php",
+  eventDetails: "/meventdetail.php",
+  eventResults: "/mgetresultevent.php",
+  runnerProfile: "/mgetresultperson.php",
+  // Any other endpoints
+};
+/* eslint-enable babel/require-config-file */
